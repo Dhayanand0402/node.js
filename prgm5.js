c7b5358fbd45c36c5b086f644a7f84d96c1d9abf
Dhayanand R <dhayanand0402@gmail.com>
2026-07-19 10:04:27 +0530

    MongoDB connection using Mongoose.

--- prgm5.js
+++ prgm5.js
@@ -1,4 +1,5 @@
-mongoose.connect('mongodb://127.0.0.1:27017/studentDB')
-.then(()=>console.log("MongoDB connected"))
-.catch(err=>console.log(err));
+const mongoose = require('mongoose');
 
+mongoose.connect('mongodb://127.0.0.1:27017/studentDB')
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
